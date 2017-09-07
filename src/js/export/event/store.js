82f6ce5b02530149cb785d0cea34e401c04bcbf3
lijialiang <lijialiang@yy.com>
2017-09-07 17:22:06 +0800

    [update] store val can not be the same value

--- src/js/export/event/store.js
+++ src/js/export/event/store.js
@@ -9,15 +9,20 @@ export default function ( _store, _observer ) {
         get: function ( target, key, receiver ) {
             return _store[ key ];
         },
+
+        // TODO: support the complicated data type
         set: function ( target, key, value, receiver ) {
-            _store[ key ] = value;
+            // can not the same value
+            if ( !( _store[ key ] && _store[ key ] == value ) ) {
+                _store[ key ] = value;
 
-            const observerStoreKey = `@store/${ key }`;
+                const observerStoreKey = `@store/${ key }`;
 
-            if ( typeof _observer[ observerStoreKey ] !== 'undefined' ) {
-                _observer[ observerStoreKey ].forEach(( handler, index ) => {
-                	handler( value );
-                });
+                if ( typeof _observer[ observerStoreKey ] !== 'undefined' ) {
+                    _observer[ observerStoreKey ].forEach(( handler, index ) => {
+                        handler( value );
+                    });
+                }
             }
         }
     });
